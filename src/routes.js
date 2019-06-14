0f4fa01ec24cf5bf06c5568a4a141cb9095fc0a4
MarceloNascc <joao.nascimento@dcomp.ufs.br>
2019-06-14 17:23:13 -0300

    back-end da omniStack sem alterações

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const routes = require('express').Router();
+const uploadConfig = require('./config/upload');
+const multer = require('multer');
+
+const upload = multer(uploadConfig);
+
+routes.get('/teste', (req, res) => {
+    return res.send(`Olá ${req.query.name}`);
+});
+
+
+const PostController = require('./controllers/PostController');
+routes.get('/post', PostController.index);
+routes.post('/post', upload.single('image'), PostController.store);
+
+const LikeController = require('./controllers/LikeController');
+routes.post('/posts/:id/like', LikeController.store);
+
+module.exports = routes;
